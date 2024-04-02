4fc22e07920fa54760d94a787da350331392cecc
fabu11 <abullarade.franco@gmail.com>
2024-04-01 20:36:11 -0700

    Implemented LuaSnippet for CMP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,21 +7,79 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
--- Custom Binds
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    -- confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
-    ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-    -- scroll up and down the documentation window
-    ['<C-Up>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-Down>'] = cmp.mapping.scroll_docs(4)
-  })
-})
+-- cmp
 
+local check_backspace = function()
+  local col = vim.fn.col "." - 1
+  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
+
+
+local cmp = require('cmp')
+local luasnip = require("luasnip")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif check_backspace() then
+        fallback()
+      else
+        fallback()
+      end
+    end, {
+    "i",
+    "s",
+  }),
+  ["<S-Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+      luasnip.jump(-1)
+    else
+      fallback()
+    end
+  end, {
+  "i",
+  "s",
+}),
+    },
+    sources = {
+      { name = 'nvim_lsp' }, -- LSP source
+      { name = 'luasnip' },  -- LuaSnip source
+      -- Other sources...
+    },
+    formatting = {
+      format = function(entry, vim_item)
+        -- Customize the appearance of the completion items here
+        vim_item.kind = string.format('%s %s', vim_item.kind, entry.source.name)
+        vim_item.menu = ({
+          nvim_lsp = "[LSP]",
+          luasnip = "luasnip"
+          -- Other sources...
+        })[entry.source.name]
+        return vim_item
+      end,
+    },
+  })
+
+-- Define your snippets here
+require('luasnip').setup()
+
+
+-- extras 
 lsp_zero.set_preferences({
     suggest_lsp_servers = true,
 })

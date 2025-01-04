d504187a1be05b86396ebb21311f05d8d1681896
fabu11 <abullarade.franco@gmail.com>
2025-01-03 21:48:31 -0800

    added esc to exit oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,29 +1,30 @@
 return {
-  {
-    'stevearc/oil.nvim',
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    config = function()
-      require("oil").setup {
-        columns = { "icon" },
-        keymaps = {
-          ["l"] = { "actions.select", mode = "n" },
-          ["h"] = { "actions.parent", mode = "n" },
-          ["<right>"] = { "actions.select", mode = "n" },
-          ["<left>"] = { "actions.parent", mode = "n" },
-        },
-        view_options = {
-          show_hidden = true,
-        },
-        float = {
-          max_width = math.floor(vim.o.columns * 0.5), -- 80% of the screen width
-          max_height = math.floor(vim.o.lines * 0.5),  -- 80% of the screen height
-          preview_split = "auto",
-          win_options = {
-            winblend = 10,
-          }
+    {
+        'stevearc/oil.nvim',
+        dependencies = { { "echasnovski/mini.icons", opts = {} } },
+        config = function()
+            require("oil").setup {
+                columns = { "icon" },
+                keymaps = {
+                    ["l"] = { "actions.select", mode = "n" },
+                    ["h"] = { "actions.parent", mode = "n" },
+                    ["<right>"] = { "actions.select", mode = "n" },
+                    ["<left>"] = { "actions.parent", mode = "n" },
+                    ["<esc>"] = { "actions.close", mode = "n" },
+                },
+                view_options = {
+                    show_hidden = true,
+                },
+                float = {
+                    max_width = math.floor(vim.o.columns * 0.5), -- 80% of the screen width
+                    max_height = math.floor(vim.o.lines * 0.5),  -- 80% of the screen height
+                    preview_split = "auto",
+                    win_options = {
+                        winblend = 10,
+                    }
 
-        }
-      }
-    end
-  }
+                }
+            }
+        end
+    }
 }
